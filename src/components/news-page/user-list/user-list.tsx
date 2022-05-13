e5571bcb4083597abd16b0cd176af34e18fa3712
summerlightningman <div8@bk.ru>
2022-05-13 22:44:03 +0300

    redux was added

---
+++ src/components/news-page/user-list/user-list.tsx
@@ -0,0 +1,10 @@
+import {FC} from 'react';
+import NewsContentStyled from "../news-content/news-content.styled";
+
+const UserList: FC = () => {
+    return <NewsContentStyled>
+        UserList works!
+    </NewsContentStyled>
+};
+
+export default UserList;
--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,50 @@
 import {createRoot} from "react-dom/client";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Provider} from "react-redux";
 import {QueryClient, QueryClientProvider} from 'react-query';
+
+import store from './redux/store';
+import Endpoint from "./endpoint";
 
 import App from "./components/app/app";
 import GlobalStyle from "./components/styled/global";
-import Endpoint from "./endpoint";
+import NavbarEndpoint from "./components/news-page/news-navbar/navbar-endpoint";
+
 import NewsPage from "./components/news-page/news-page";
 import AuthPage from "./components/auth-page/auth-page";
 import SignInForm from "./components/auth-page/sign-in-form/sign-in-form";
 import SignUpForm from "./components/auth-page/sign-up-form/sign-up-form";
 import NotFound from "./components/not-found/not-found";
-import NavbarEndpoint from "./components/news-page/news-navbar/navbar-endpoint";
 import NewsContent from "./components/news-page/news-content/news-content";
 import TagList from "./components/news-page/tag-list/tag-list";
+import UserList from "./components/news-page/user-list/user-list";
+
 
 const queryClient = new QueryClient();
 
 const root = createRoot(document.querySelector('#root') as HTMLElement);
 root.render(
     <QueryClientProvider client={queryClient}>
-        <GlobalStyle/>
-        <BrowserRouter>
-            <Routes>
-                <Route path={Endpoint.ROOT} element={<App/>}>
-                    <Route path={Endpoint.ROOT} element={<NewsPage/>}>
-                        <Route path={NavbarEndpoint.ALL} element={<NewsContent/>}/>
-                        <Route path={NavbarEndpoint.SUBSCRIBED} element={<NewsContent filterByTag={true}/>}/>
-                        <Route path={NavbarEndpoint.TAG_LIST} element={<TagList/>}/>
-                        <Route path={NavbarEndpoint.USER_LIST} element={<NewsContent/>}/>
-                        <Route path={NavbarEndpoint.NEW_POST} element={<NewsContent/>}/>
+        <Provider store={store}>
+            <GlobalStyle/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={Endpoint.ROOT} element={<App/>}>
+                        <Route path={Endpoint.ROOT} element={<NewsPage/>}>
+                            <Route path={NavbarEndpoint.ALL} element={<NewsContent/>}/>
+                            <Route path={NavbarEndpoint.SUBSCRIBED} element={<NewsContent filterByTag={true}/>}/>
+                            <Route path={NavbarEndpoint.TAG_LIST} element={<TagList/>}/>
+                            <Route path={NavbarEndpoint.USER_LIST} element={<UserList/>}/>
+                            <Route path={NavbarEndpoint.NEW_POST} element={<NewsContent/>}/>
+                        </Route>
+                        <Route path={Endpoint.AUTH} element={<AuthPage/>}>
+                            <Route index element={<SignInForm/>}/>
+                            <Route path={Endpoint.SIGN_UP} element={<SignUpForm/>}/>
+                        </Route>
                     </Route>
-                    <Route path={Endpoint.AUTH} element={<AuthPage/>}>
-                        <Route index element={<SignInForm/>}/>
-                        <Route path={Endpoint.SIGN_UP} element={<SignUpForm/>}/>
-                    </Route>
-                </Route>
-                <Route path={Endpoint.NOT_FOUND} element={<NotFound/>}/>
-            </Routes>
-        </BrowserRouter>
+                    <Route path={Endpoint.NOT_FOUND} element={<NotFound/>}/>
+                </Routes>
+            </BrowserRouter>
+        </Provider>
     </QueryClientProvider>
 )
